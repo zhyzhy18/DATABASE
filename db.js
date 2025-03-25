5242595963c4f57c193da3c5d54b288a7f9668b0
Chalex <177287665+chalexmarmol@users.noreply.github.com>
2025-03-25 10:51:45 +0800

    fix the studentportal.sql

--- db.js
+++ db.js
@@ -1,14 +1,15 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const mysqlPool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'zyrusjaybagay',
-    database: 'studentportal',
+	host: "localhost",
+	user: "root",
+	password: "",
+	database: "studentportal",
 });
 
-mysqlPool.query("SELECT 1")
-.then(() => console.log('db connection succeeded.'))
-.catch(err => console.log('db connection failed. \n' + err))
+mysqlPool
+	.query("SELECT 1")
+	.then(() => console.log("db connection succeeded."))
+	.catch((err) => console.log("db connection failed. \n" + err));
 
 module.exports = mysqlPool;
